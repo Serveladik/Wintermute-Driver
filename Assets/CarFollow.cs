ee4682aa1cbe50d117c198a641bcd3de6420bf59
Vladyslav <serveladik@gmail.com>
2020-09-02 22:07:37 +0300

    ффф

---
+++ Assets/CarFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarFollow : MonoBehaviour
+{
+    public Transform TruckPos;
+    public float carSpeed = 40f;
+    const float dist = 5f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(TruckPos.position);
+        if((transform.position - TruckPos.position).magnitude > dist)
+        {
+            transform.Translate(0f,0f,carSpeed*Time.deltaTime);
+        }
+    }
+}
