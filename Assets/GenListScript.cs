b6f26eb791d2c793b83f9432ca78b5163632d4db
Vladyslav <serveladik@gmail.com>
2020-09-02 18:18:01 +0300

    GenLocationFixed

---
+++ Assets/GenListScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenListScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public List <GameObject> locationList = new List<GameObject>();
+   public int genCount;
+}
--- Assets/GenerateLocationScript.cs
+++ Assets/GenerateLocationScript.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class GenerateLocationScript : MonoBehaviour
 {
-    List <GameObject> locationList = new List<GameObject>();
+    public GenListScript genObj;
     GameObject genLocation;
-    private int genCount=0;
+    
     private Vector3 nextLocation = new Vector3 (0,0,175);
     public GameObject[] locations;
    void OnTriggerEnter(Collider generateTrigger)
    {
        if(generateTrigger.gameObject.tag == "Truck")
        {
-           locationList.Add((GameObject) Instantiate(locations[Random.Range(0,1)],gameObject.transform.position+nextLocation,Quaternion.Euler(0,90,0)) as GameObject);
-           Destroy(locationList[genCount],10f);
+           genObj.locationList.Add((GameObject) Instantiate(locations[Random.Range(0,1)],gameObject.transform.position+nextLocation,Quaternion.Euler(0,90,0)) as GameObject);
+           Destroy(genObj.locationList[genObj.genCount-1]);
+           genObj.genCount++;
        }
        
    }
